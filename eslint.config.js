50fe167dd9f9f4c305cf6355a75631a60b67d1e2
Tarasov_Ilya <ilya.tarasov>
2026-03-22 13:51:57 +0400

    configurate vscode

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,7 @@ export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     plugins: { js },
-    extends: ['js/recommended'],
+    extends: ['js/recommended', 'prettier'],
     languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
